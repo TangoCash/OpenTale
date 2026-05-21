9159b583c606645d2a0541fe9be40a17cc9adaff
TangoCash <eric@loxat.de>
2026-05-21 11:06:51 +0200

    add start.bat for windows

---
+++ start.bat
@@ -0,0 +1,14 @@
+@echo off
+REM ===========================================
+REM Start script for Writingway
+REM ===========================================
+
+REM Activate the virtual environment.
+echo Activating virtual environment...
+call .venv\Scripts\activate
+
+REM Run the main Python script.
+echo Running main.py...
+python web_app.py
+
+pause
